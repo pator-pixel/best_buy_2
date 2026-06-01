a4bb52a7e682538db8efc87ccc9532c5e3f7ed00
pator-pixel <pator@gmx.net>
2026-06-01 12:07:57 +0200

    Verbesserungen in main

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def list_products(best_buy):
 
     print("------")
     for index, product in enumerate(all_products, start=1):
-        print(f"{index}. {product.name}, Price: {product.price}, Quantity: {product.quantity}")
+        print(f"{index}. ", end="")
+        product.show()
     print("------")
 
 
@@ -79,10 +80,23 @@ def main():
     product_list = [
         products.Product("MacBook Air M2", price=1450, quantity=100),
         products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-        products.Product("Google Pixel 7", price=500, quantity=250)
+        products.Product("Google Pixel 7", price=500, quantity=250),
+        products.NonStockedProduct("Windows License", price=125),
+        products.LimitedProduct("Shipping", price=10, quantity=250, maximum=1)
     ]
 
+    # Create promotion catalog
+    second_half_price = promotions.SecondHalfPrice("Second Half price!")
+    third_one_free = promotions.ThirdOneFree("Third One Free!")
+    thirty_percent = promotions.PercentDiscount("30% off!", percent=30)
+
+    # Add promotions to products
+    product_list[0].set_promotion(second_half_price)
+    product_list[1].set_promotion(third_one_free)
+    product_list[3].set_promotion(thirty_percent)
+
     best_buy = store.Store(product_list)
+
     start(best_buy)
 
 
